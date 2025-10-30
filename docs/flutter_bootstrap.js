f41d450ca1d41d9110b1aa62d61af3ed8b44fd45
ARYAN <dobariyanimsh14@gmail.com>
2025-10-30 23:10:04 +0530

    fix: base href for github pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1285438653"
+    serviceWorkerVersion: "2679547935"
   }
 });
